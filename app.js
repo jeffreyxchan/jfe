222e0fcbd2bba1402dbea8b562752f88c1aeb1a3
Jeffrey Chan <jeffschan97@gmail.com>
2019-09-11 21:37:39 -0700

    Change

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const config = require("./config.js")
 const app = express()
 const apiProxy = httpProxy.createProxyServer()
 
+// Use the morgan logging package with it's "common" setting.
 app.use(morgan("common"))
 
+// Status endpoint to test if the proxy server is running and can be reached.
 app.get("/status", (req, res) => {
-	res.send("Hello, World!")
+	res.send("Ok")
 })
 
+// Configure all the proxy paths specified in config.js
 for (s of config.services) {
 	app.all(s.PROXY_PATH, function(req, res) {
 		apiProxy.web(req, res, { target: s.TARGET })
